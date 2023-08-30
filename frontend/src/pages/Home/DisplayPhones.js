807eef574f6ddbcd2e3fc218109d984020b976fb
Tony Vo <tonyvo249@gmail.com>
2023-08-30 15:12:47 +1000

    include add to cart btn to display phones

--- frontend/src/pages/Home/DisplayPhones.js
+++ frontend/src/pages/Home/DisplayPhones.js
@@ -1,21 +1,34 @@
+import { Link } from "react-router-dom"
 import '../../css/global.css';
 import '../../css/home-state.css';
-import { Link } from "react-router-dom"
+import ShoppingCartIcon from "../../assets/shopping-cart.png";
 
 function DisplayPhones({ phones }) {
+
+	function addToCart(event, phoneUID) {
+		// event.stopPropagation();
+  	console.log(phoneUID);
+	}
+
 	return (
 		<div className='categories'>
 			{phones && phones.length > 0 ? (
 				phones.map((phone) => {
 					return (
 						<div key={phone.uid}>
-              <Link to={"/item/" + phone.uid} className='phone-link-style'>
 								<div className='phone-listing'>
-									<img className='phone-img' src={`${phone.image}`} alt={phone.image}/>
-									<p className='title'>{phone.title.length > 30 ? phone.title.slice(0, 30) : phone.title}...</p>
-									<p className='price'>${phone.price}</p>
+              		<Link to={"/item/" + phone.uid} className='phone-link-style'>
+										<img className='phone-img' src={`${phone.image}`} alt={phone.image}/>
+										<p className='title'>{phone.title.length > 30 ? phone.title.slice(0, 30) : phone.title}...</p>
+									</Link>
+									<div className="price-cart">
+              			<Link to={"/item/" + phone.uid} className='phone-link-style'>
+											<p className='price'>${phone.price}</p>
+										</Link>
+										{/* <button className="quick-cart-btn" onClick={(event) => addToCart(event, phone.uid)}></button> */}
+										<img className='quick-cart-img' src={ShoppingCartIcon} alt={ShoppingCartIcon} onClick={(event) => addToCart(event, phone.uid)}/>
+									</div>
 								</div>
-							</Link>
 						</div>
 					);
 				})
