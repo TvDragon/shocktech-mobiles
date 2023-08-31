dea4d3a521a985046be36b11da6f1ba6a82be6cb
Tony Vo <tonyvo249@gmail.com>
2023-08-31 21:07:11 +1000

    working on product page

--- frontend/src/pages/Product/ProductState.js
+++ frontend/src/pages/Product/ProductState.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import "../../css/global.css";
+import "../../css/product-state.css";
+import HeaderBar from "../../components/HeaderBar";
 
 function ProductState() {
   const location = useLocation();
@@ -23,8 +25,21 @@ function ProductState() {
   }, [uid]);
 
   return (
-    <div key={phone.uid}>
-      {phone.title}
+    <div className="content">
+      <HeaderBar />
+      <div key={phone.uid} id="product">
+        <div id="product-info">
+          <div id="product-phone-img">
+            <img src={`${phone.image}`} alt={phone.image}/>
+          </div>
+          <div id="product-desc">
+            <p id="title" className="text-color">{phone.title}</p>
+            <p id="item-number">Item Number: {phone.uid}</p>
+          </div>
+        </div>
+        <div id="reviews">
+        </div>
+      </div>
     </div>
   )
 }
