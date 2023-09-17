210ce36d4be45d509de5ab0e7e965e6fe9718285
Tony Vo <tonyvo249@gmail.com>
2023-09-17 15:34:42 +1000

    sign in and sign up form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeState from "./pages/Home/HomeState";
 import SearchState from "./pages/Search/SearchState";
 import ProductState from "./pages/Product/ProductState";
+import AccountForm from "./pages/Account/AccountForm";
 import ErrorState from "./pages/Error/ErrorState";
 
 import "./css/global.css";
@@ -14,6 +15,7 @@ function App() {
             <Route path="/" element={<HomeState />} />
             <Route path="/search" element={<SearchState />} />
             <Route path="/product" element={<ProductState />} />
+            <Route path="/account" element={<AccountForm />} />
             <Route path="*" element={<ErrorState />} />
         </Routes>
       </BrowserRouter>
---
+++ frontend/src/pages/Account/AccountForm.js
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import "../../css/global.css";
+import "../../css/account-form.css";
+import HeaderBar from "../../components/HeaderBar";
+
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
+
+function AccountForm() {
+  const [showLoginForm, setShowLoginForm] = useState(true);
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function displayLoginForm() {
+    setShowLoginForm(true);
+  }
+
+  function displayCreateAccountForm() {
+    setShowLoginForm(false);
+  }
+
+  function signIn() {
+    if (email === "" || password === "") {
+      MySwal.fire({
+        title: "Fields Cannot Be Empty",
+        icon: "error",
+      });
+    } else {
+
+    }
+  }
+
+  function signUp() {
+    if (fname === "" || lname === "" || email === "" || password === "") {
+      MySwal.fire({
+        title: "Fields Cannot Be Empty",
+        icon: "error",
+      });
+    } else {
+
+    }
+  }
+  
+  const changeFname = (e) => {
+    setFname(e.target.value);
+  }
+  
+  const changeLname = (e) => {
+    setLname(e.target.value);
+  }
+
+  const changeEmail = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const changePassword = (e) => {
+    setPassword(e.target.value);
+  }
+  
+  return (
+    <div className="content">
+      <HeaderBar />
+      <div id="account-form">
+        <div className="header-account-form">
+          <button onClick={displayLoginForm}>LOGIN</button>
+          <div className="vert-line"></div>
+          <button onClick={displayCreateAccountForm}>CREATE ACCOUNT</button>
+          </div>
+          <hr></hr>
+          <div id="forms">
+            {showLoginForm ? (
+              <div id="account-form">
+                <label>Email</label><span className="asterik">*</span><br></br>
+                <input type="email" onChange={changeEmail}></input><br></br><br></br>
+                <label>Password</label><span className="asterik">*</span><br></br>
+                <input type="password" onChange={changePassword}></input><br></br><br></br>
+                <div className="center-container">
+                  <button className="shared-btn" onClick={() => {signIn()}}>Sign In</button>
+                </div>
+                <div className="center-container">
+                  <button className="text-btn ps-fs-20"><u>Forgot your password?</u></button>
+                </div>
+            </div>
+          ):
+            <div id="account-form">
+              <p className="pfd-fs-28">Personal Information</p><br></br>
+              <label>First Name</label><span className="asterik">*</span><br></br>
+              <input type="text" onChange={changeFname}></input><br></br><br></br>
+              <label>Last Name</label><span className="asterik">*</span><br></br>
+              <input type="text" onChange={changeLname}></input><br></br><br></br>
+              <p className="pfd-fs-28">Sign-In Information</p><br></br>
+              <label>Email</label><span className="asterik">*</span><br></br>
+              <input type="email" onChange={changeEmail}></input><br></br><br></br>
+              <label>Password</label><span className="asterik">*</span><br></br>
+              <input type="password" onChange={changePassword}></input><br></br><br></br>
+              <div className="center-container">
+                <button className="shared-btn" onClick={() => {signUp()}}>Create Account</button>
+              </div>
+            </div>}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AccountForm;
